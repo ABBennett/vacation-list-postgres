33b071d54f5450e72f01cce47c181cd64db003df
AlexBennett <alexanderbondbennett@gmail.com>
2015-11-25 08:53:05 -0500

    Added readme

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO vacations (destination) VALUES ('COW-LOON');
+INSERT INTO comments (body, vacation_id) VALUES ('It is crowded', 1);
+INSERT INTO comments (body, vacation_id) VALUES ('It is big', 1);
+INSERT INTO comments (body, vacation_id) VALUES ('city like no other', 2);
